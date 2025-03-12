fda38cdd7690bde732f9f8d3f952ec4f0435bc10
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-12 23:34:15 +0530

    updating ./learn_shell_2.0

--- print.sh
+++ print.sh
@@ -1,1 +1,11 @@
+# to print a message we use echo command
 echo "Hello World"
+
+# to print in colors
+
+echo -e "\e[31mHello in Red color\e[0m"
+echo -e "\e[32mHello in Green color\e[0m"
+echo -e "\e[33mHello in Yellow color\e[0m"
+echo -e "\e[34mHello in Blue color\e[0m"
+echo -e "\e[35mHello in Magenta color\e[0m"
+echo -e "\e[36mHello in Cyan color\e[0m"
